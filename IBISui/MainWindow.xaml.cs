118f3df3a40bfab97356545cb89522c7fd6f14c4
Peter Recktenwald <p.recktenwald@gmail.com>
2015-09-03 10:57:03 +0200

    IBIS send  RAW , Character count

--- IBISui/MainWindow.xaml.cs
+++ IBISui/MainWindow.xaml.cs
@@ -22,10 +22,15 @@ namespace IBISui
         public MainWindow()
         {
             InitializeComponent();
+            getSerialPorts();
             myTimer = new Timer(10000); // Set up the timer for 10 seconds
                                         //
                                         // Type "_timer.Elapsed += " and press tab twice.
                                         //
+            mnuTimer5s.IsChecked = false;
+            mnuTimer10s.IsChecked = true;
+            mnuTimer20s.IsChecked = false;
+            mnuTimer30s.IsChecked = false;
             myTimer.Elapsed += new ElapsedEventHandler(myTimer_Elapsed);
         }
 
@@ -90,8 +95,8 @@ namespace IBISui
             return ret;
         }
 
-        private void mnuSerial_Click(object sender, RoutedEventArgs e)
-        {
+        private void getSerialPorts()
+        { 
             mnuSerial.Items.Clear();
             var myPort = SerialPort.GetPortNames();
             foreach (string port in myPort)
@@ -107,6 +112,10 @@ namespace IBISui
                 }
             }
         }
+
+        private void mnuSerial_Click(object sender, RoutedEventArgs e)
+        {
+            getSerialPorts();        }
 
         private void mnuSerialPort_Click(object sender, RoutedEventArgs e)
         {
@@ -126,6 +135,7 @@ namespace IBISui
 
         private void sendIBIS()
         {
+
             if (isOpen == false)
             {
                 comm.Parity = Parity.None.ToString();
@@ -139,7 +149,6 @@ namespace IBISui
             }
             if (isOpen == true)
             {
-                //btnSenden.
                 if (cbLiniennummer.IsChecked == true)
                 {
                     comm.WriteIbisData("l" + txtLiniennummer.Text);
@@ -150,15 +159,19 @@ namespace IBISui
                 }
                 if (cbZieltext1_2.IsChecked == true && cbZieltext3_4.IsChecked == true)
                 {
-                    comm.WriteIbisData("zA4" + centerString(txtZieltext1.Text, 16) + centerString(txtZieltext2.Text, 16) + centerString(txtZieltext3.Text, 16) + centerString(txtZieltext4.Text, 16));
+                    comm.WriteIbisData("zA4" + centerString(txtZieltext1.Text, txtZieltext1.MaxLength) + centerString(txtZieltext2.Text, txtZieltext2.MaxLength) + centerString(txtZieltext3.Text, txtZieltext3.MaxLength) + centerString(txtZieltext4.Text, txtZieltext4.MaxLength));
                 }
                 else if (cbZieltext1_2.IsChecked == true && cbZieltext3_4.IsChecked == false)
                 {
-                    comm.WriteIbisData("zA2" + centerString(txtZieltext1.Text, 16) + centerString(txtZieltext2.Text, 16));
+                    comm.WriteIbisData("zA2" + centerString(txtZieltext1.Text, txtZieltext1.MaxLength) + centerString(txtZieltext2.Text, txtZieltext2.MaxLength));
                 }
                 if (cbHaltestelle.IsChecked == true)
                 {
-                    comm.WriteIbisData("v" + centerString(txtHaltestelle.Text, 24));
+                    comm.WriteIbisData("v" + centerString(txtHaltestelle.Text, txtHaltestelle.MaxLength));
+                }
+                if (cbRAW.IsChecked == true)
+                {
+                    comm.WriteIbisData(centerString(txtRAW.Text, txtRAW.MaxLength));
                 }
             }
         }
@@ -174,6 +187,7 @@ namespace IBISui
             {
                 isTimer = true;
                 myTimer.Enabled = true; // Enable it
+                sendIBIS();
             }
             else
             {
@@ -197,6 +211,159 @@ namespace IBISui
         private void mnuTimer_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void mnuHSA_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cbHaltestelle_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbHaltestelle.IsChecked == true)
+            {
+                txtHaltestelle.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtHaltestelle.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void cbZieltext3_4_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbZieltext3_4.IsChecked == true)
+            {
+                txtZieltext3.Visibility = Visibility.Visible;
+                txtZieltext4.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtZieltext3.Visibility = Visibility.Collapsed;
+                txtZieltext4.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void cbZieltext1_2_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbZieltext1_2.IsChecked == true)
+            {
+                txtZieltext1.Visibility = Visibility.Visible;
+                txtZieltext2.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtZieltext1.Visibility = Visibility.Collapsed;
+                txtZieltext2.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void cbLiniennummer_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbLiniennummer.IsChecked == true)
+            {
+                txtLiniennummer.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtLiniennummer.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void cbSonderzeichen_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbSonderzeichen.IsChecked == true)
+            {
+                txtSonderzeichen.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtSonderzeichen.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void cbRAW_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbRAW.IsChecked == true)
+            {
+                txtRAW.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtRAW.Visibility = Visibility.Collapsed;
+            }
+
+        }
+
+        private void mnuTimer5s_Click(object sender, RoutedEventArgs e)
+        {
+            myTimer.Interval = 5000;
+            mnuTimer5s.IsChecked = true;
+            mnuTimer10s.IsChecked = false;
+            mnuTimer20s.IsChecked = false;
+            mnuTimer30s.IsChecked = false;
+
+        }
+
+        private void mnuTimer10s_Click(object sender, RoutedEventArgs e)
+        {
+            myTimer.Interval = 10000;
+            mnuTimer5s.IsChecked = false;
+            mnuTimer10s.IsChecked = true;
+            mnuTimer20s.IsChecked = false;
+            mnuTimer30s.IsChecked = false;
+
+        }
+
+        private void mnuTimer20s_Click(object sender, RoutedEventArgs e)
+        {
+            myTimer.Interval = 20000;
+            mnuTimer5s.IsChecked = false;
+            mnuTimer10s.IsChecked = false;
+            mnuTimer20s.IsChecked = true;
+            mnuTimer30s.IsChecked = false;
+
+        }
+
+        private void mnuTimer30s_Click(object sender, RoutedEventArgs e)
+        {
+            myTimer.Interval = 30000;
+            mnuTimer5s.IsChecked = false;
+            mnuTimer10s.IsChecked = false;
+            mnuTimer20s.IsChecked = false;
+            mnuTimer30s.IsChecked = true;
+
+        }
+
+        private void txtZieltext1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblZieltext1.Content = txtZieltext1.Text.Length;
+        }
+
+        private void txtZieltext2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblZieltext2.Content = txtZieltext2.Text.Length;
+
+        }
+
+        private void txtZieltext3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblZieltext3.Content = txtZieltext3.Text.Length;
+        }
+
+        private void txtZieltext4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblZieltext4.Content = txtZieltext4.Text.Length;
+        }
+
+        private void txtHaltestelle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblHaltestelle.Content = txtHaltestelle.Text.Length;
+        }
+
+        private void txtRAW_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblRAW.Content = txtRAW.Text.Length;
         }
     }
 }
